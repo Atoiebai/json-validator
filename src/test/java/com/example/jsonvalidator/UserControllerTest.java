4775c8c6ec1055f119a890883b4b6dfee999721e
Atoiebai <yusubov9999@mail.ru>
2021-11-22 16:43:30 +0500

    FIX: deleted redundant annotations

--- src/test/java/com/example/jsonvalidator/UserControllerTest.java
+++ src/test/java/com/example/jsonvalidator/UserControllerTest.java
@@ -20,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 //config for WebTestClient
 //@WebFluxTest
-//@SpringBootTest
 class UserControllerTest {
 
     @Autowired
